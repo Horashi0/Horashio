8ecbec6841712654ec3a8c57c066f9e4a38cb092
Horashio <Horashi0@users.noreply.github.com>
2024-04-07 12:55:49 +0100

    Updated streaming section of website!
    Elements can now move independently of each other, will be adding youtube videos soon

--- JS/videoControl.js
+++ JS/videoControl.js
@@ -1,15 +1,15 @@
-function addElement() {
+function addElement(parentElement) {
     var videoContainer = document.createElement("div");
     var videoHandler = document.createElement("div");
     var videoDiv = document.createElement("div");
 
     videoDiv.style.width = "100%";
     videoDiv.style.backgroundColor = "red";
+    videoDiv.style.cursor = "se-resize";
 
     videoContainer.style.backgroundColor = "white";
-    videoContainer.style.position = "relative";
-    videoContainer.style.cursor = "se-resize";
-
+    videoContainer.style.position = "absolute";
+    
     videoHandler.style.width = "100%";
     videoHandler.style.height = "50px";
     videoHandler.style.backgroundColor = "blue";
@@ -17,8 +17,8 @@ function addElement() {
 
     videoContainer.appendChild(videoHandler);
     videoContainer.appendChild(videoDiv);
-    document.getElementById("contentArea").appendChild(videoContainer);
-
+    parentElement.appendChild(videoContainer);
+ 
     videoContainer.style.width = "50%";
     videoContainer.style.left = "0px"; // Initial position
     videoContainer.style.top = "0px"; // Initial position
@@ -28,20 +28,22 @@ function addElement() {
    }, 0);
     
     // Event listeners for dragging
-    var offsetX, offsetY;
+    var initialX, initialY;
     var isDragging = false;
 
     videoHandler.addEventListener('mousedown', function(e) {
         isDragging = true;
-        offsetX = e.clientX - videoContainer.offsetLeft;
-        offsetY = e.clientY - videoContainer.offsetTop;
+        initialX = e.clientX - videoContainer.offsetLeft;
+        initialY = e.clientY - videoContainer.offsetTop;
         videoContainer.classList.add('grabbed');
     });
 
     document.addEventListener('mousemove', function(e) {
         if (isDragging) {
-            videoContainer.style.left = (e.clientX - offsetX) + 'px';
-            videoContainer.style.top = (e.clientY - offsetY) + 'px';
+            var newX = e.clientX - initialX;
+            var newY = e.clientY - initialY;
+            videoContainer.style.left = newX + 'px';
+            videoContainer.style.top = newY + 'px';
         }
     });
 
@@ -49,10 +51,45 @@ function addElement() {
         isDragging = false;
         videoContainer.classList.remove('grabbed');
     });
-    
+
+    videoDiv.addEventListener('mousedown', initResize, false);
+
+    function initResize(e) {
+        window.addEventListener('mousemove', Resize, false);
+        window.addEventListener('mouseup', stopResize, false);
+    }
+
+    function Resize(e) {
+        videoContainer.style.width = (e.clientX - videoContainer.offsetLeft) + 'px';
+        adjustAspectRatio(videoDiv);
+    }
+
+    function stopResize(e) {
+        window.removeEventListener('mousemove', Resize, false);
+        window.removeEventListener('mouseup', stopResize, false);
+    }
 };
 
-addElement();
+var contentArea1 = document.createElement("div");
+var contentArea2 = document.createElement("div");
+var contentArea3 = document.createElement("div");
+var contentArea4 = document.createElement("div");
+var contentArea5 = document.createElement("div");
+var contentArea6 = document.createElement("div");
+
+document.getElementById("contentArea").appendChild(contentArea1)
+document.getElementById("contentArea").appendChild(contentArea2)
+document.getElementById("contentArea").appendChild(contentArea3)
+document.getElementById("contentArea").appendChild(contentArea4)
+document.getElementById("contentArea").appendChild(contentArea5)
+document.getElementById("contentArea").appendChild(contentArea6)
+
+addElement(contentArea1);
+addElement(contentArea2);
+addElement(contentArea3);
+addElement(contentArea4);
+addElement(contentArea5);
+addElement(contentArea6);
 
 function adjustAspectRatio(container) {
     var width = container.offsetWidth;
