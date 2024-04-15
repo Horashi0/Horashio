be8e9f22dec76a6f4bde4ca5307d67e1de2922e6
Horashio <Horashi0@users.noreply.github.com>
2024-04-15 01:37:34 +0100

    Major changes, added master volume control, moved from innerHtml to youtube player control
    innerHTML approach for streaming content didnt provide enough customisability so I have moved to the Youtube Player Api

--- JS/videoControl.js
+++ JS/videoControl.js
@@ -1,6 +1,6 @@
 var highestZIndex = 1;
 var idArray = [];
-
+var volumeState;
 
 function addCheckboxListeners() {
     var checkboxes = document.querySelectorAll('input[type="checkbox"]');
@@ -19,9 +19,6 @@ function refresh() {
     var refresh = document.getElementById("refresh");
     
     refresh.addEventListener("click", function() {
-        on.style.display = "none";
-        off.style.display = "block";
-        console.log(idArray);
         idArray.forEach(function(idArray) {
             var videoId = idArray + "Div";
             var videoDiv = document.getElementById(videoId);
@@ -42,6 +39,55 @@ function refresh() {
     });
 }
 
+function volume() {
+    var volumeOn = document.getElementById("volumeOn");
+    var volumeOff = document.getElementById("volumeOff");
+
+    volumeOn.style.display = "none";
+    volumeOff.style.display = "block";
+
+    if (typeof(volumeState) === "undefined") {
+        volumeState = "mute";
+    } else {
+
+    }
+    
+    
+    volumeOn.addEventListener("click", function() {
+        volumeState = "mute";
+        volumeOn.style.display = "none";
+        volumeOff.style.display = "block";
+
+        idArray.forEach(function(idArray) {
+            var videoId = idArray + "Div";
+            var videoDiv = document.getElementById(videoId);
+            if(videoDiv) {
+                var iframe = videoDiv.querySelector("iframe");
+                if(iframe) {
+                    iframe.contentWindow.postMessage('{"event":"command","func":"mute","method":"setVolume","value":0}', '*');                    
+                }
+            }
+        })
+    })
+
+    volumeOff.addEventListener("click", function() {
+        volumeState = "unmute";
+        volumeOn.style.display = "block";
+        volumeOff.style.display = "none";
+
+        idArray.forEach(function(idArray) {
+            var videoId = idArray + "Div";
+            var videoDiv = document.getElementById(videoId);
+            if(videoDiv) {
+                var iframe = videoDiv.querySelector("iframe");
+                if(iframe) {
+                    iframe.contentWindow.postMessage('{"event":"command","func":"unMute","method":"setVolume","value":100}', '*');
+                }
+            }
+        })
+
+    })
+}
 
 function stopStream(id) {
     id = id + "Div";
@@ -82,7 +128,9 @@ function addElement(url, id) {
     overlayDiv.style.height = "100%";
     overlayDiv.style.zIndex = "9999999";
 
+   
     videoDiv.style.width = "100%";
+    videoDiv.style.height = "100%";
     videoDiv.style.cursor = "se-resize";
     videoDiv.style.padding = "0px 3px 3px 3px";
     videoDiv.style.backgroundColor = "#141723";
@@ -105,17 +153,12 @@ function addElement(url, id) {
     xDiv.style.zIndex = videoHandler.zIndex++;
     xDiv.innerHTML = "<span class='material-symbols-outlined'>close</span>";
 
-   
-
-
-    
-
     videoHandler.appendChild(xDiv);
     videoContainer.appendChild(videoHandler);
     videoContainer.appendChild(videoDiv);
     parentElement.appendChild(videoContainer);
  
-    videoContainer.style.width = "30%";
+    videoContainer.style.width = "25%";
     videoContainer.style.left = "0px"; // Initial position
     videoContainer.style.top = "0px"; // Initial position
     videoContainer.style.zIndex = highestZIndex++;
@@ -134,16 +177,41 @@ function addElement(url, id) {
 
     videoHandler.appendChild(titleDiv);
 
+   
 
    setTimeout(function() {
-    adjustAspectRatio(videoDiv, url);
+    adjustAspectRatio(videoDiv);
    }, 0);
 
  
-   videoDiv.innerHTML = '<iframe width="100%" height="100%" src="' + url + '?&mute=1&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+    //videoDiv.innerHTML = '<iframe width="100%" height="100%" src="' + url + '?&mute=1&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+    var wrapper = document.createElement('div');
+    wrapper.style.width = '100%';
+    wrapper.style.height = '100%';
+    videoDiv.appendChild(wrapper);
+    
+    var player = new YT.Player(wrapper, {
+        height: "100%",
+        width: "100%",
+        videoId: url,
+        playerVars: {
+            autoplay: 1, // Autoplay the video
+            playsinline: 1, // Play inline on mobile devices
+            frameborder: 0,
+            allow: allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",
+            allowfullscreen: true,
+        },
+        events: {
+            'onReady': function(event) {
+                if(volumeState == "mute") {
+                    event.target.mute();
+                } else {
+                    event.target.unMute();
+                }
+            }
+        }
+   });
 
-   
-   
    xDiv.addEventListener('mousedown', function(e) {
         var videoId = videoDiv.parentNode.id.replace('Div', '');    
         stopStream(videoId);
@@ -222,7 +290,7 @@ function addElement(url, id) {
     }
 };
 
-function adjustAspectRatio(container, url) {
+function adjustAspectRatio(container) {
     var width = container.offsetWidth;
     var height = width * (9/16);
     container.style.height = height + 'px';
@@ -231,5 +299,6 @@ function adjustAspectRatio(container, url) {
 window.addEventListener('load', function() {
     addCheckboxListeners();
     refresh();
+    volume();
 }); 
 
